979959177838539e6753c918de6ae54918ce2482
xuhonggui <790786581@qq.com>
2017-03-29 10:52:08 +0800

    修改图片

--- HGTimerButtonDemo/HGTimerButtonDemo/ViewController.m
+++ HGTimerButtonDemo/HGTimerButtonDemo/ViewController.m
@@ -25,24 +25,18 @@
 
 - (void)initialize {
     
-//    HGTimerButton *timerButton = [HGTimerButton buttonWithType:UIButtonTypeCustom];
-//    timerButton.frame = CGRectMake(40, CGRectGetHeight(self.view.frame)/2 - 50, CGRectGetWidth(self.view.frame) - 80, 40);
-//    timerButton.layer.borderWidth = 0.5;
-//    timerButton.layer.borderColor = [UIColor lightGrayColor].CGColor;
-//    timerButton.titleLabel.font = [UIFont systemFontOfSize:18];
-//    [timerButton setTitle:@"获取验证码" forState:UIControlStateNormal];
-//    [timerButton setTitleColor:[UIColor colorWithRed:0/255.0 green:174.0/255.0 blue:235.0/255.0 alpha:1.0] forState:UIControlStateNormal];
-//    [timerButton setDisabledTitleFormat:@"重新获取(&s)"];
-//    [timerButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];
-//    [timerButton addTarget:self action:@selector(timerButtonClick:) forControlEvents:UIControlEventTouchUpInside];
-//    [self.view addSubview:timerButton];
-
     HGTimerButton *timerButton = [HGTimerButton buttonWithType:UIButtonTypeCustom];
     timerButton.frame = CGRectMake(40, CGRectGetHeight(self.view.frame)/2 - 50, CGRectGetWidth(self.view.frame) - 80, 40);
+    timerButton.layer.borderWidth = 0.5;
+    timerButton.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    timerButton.titleLabel.font = [UIFont systemFontOfSize:18];
     [timerButton setTitle:@"获取验证码" forState:UIControlStateNormal];
-    [timerButton setBackgroundColor:[UIColor colorWithRed:0/255.0 green:174.0/255.0 blue:235.0/255.0 alpha:1.0]];
+    [timerButton setTitleColor:[UIColor colorWithRed:0/255.0 green:174.0/255.0 blue:235.0/255.0 alpha:1.0] forState:UIControlStateNormal];
+    [timerButton setDisabledTitleFormat:@"重新获取(&s)"];
+    [timerButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];
     [timerButton addTarget:self action:@selector(timerButtonClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:timerButton];
+
 }
 
 - (IBAction)timerButtonClick:(HGTimerButton *)sender {
